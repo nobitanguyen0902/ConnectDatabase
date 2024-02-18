f8d2e4eb7898f58bea1556f5d7fd2b9840c7c3b2
Be Nguyen <benguyen183@gmail.com>
2024-02-18 18:22:41 +0700

    edit text

--- Business.Web/ClientApp/src/AppRoutes.js
+++ Business.Web/ClientApp/src/AppRoutes.js
@@ -1,6 +1,7 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { Index } from "./news/Index";
 
 const AppRoutes = [
   {
@@ -14,6 +15,10 @@ const AppRoutes = [
   {
     path: '/fetch-data',
     element: <FetchData />
+  },
+  {
+    path: '/index',
+    element: <Index />
   }
 ];
 
